38ff70c5237b0b8626f0a4b697491093329fa499
vigneshbharathi68 <vigneshbharathi68@hotmail.com>
2022-06-16 18:50:40 +0530

    removed ignore files

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ let express = require('express');
 let app = express();
 console.log('Hello World')
 
-app.use('/public', express.static(__dirname + '/public'))
+app.use('/media', express.static(__dirname + '/public'))
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/views/index.html');
